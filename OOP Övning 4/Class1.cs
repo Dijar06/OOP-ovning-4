0b25b5061cc0c335e5669ad3ba89d70ab0986508
Dijar06 <dize06001@utb.vaxjo.se>
2024-04-05 13:23:51 +0200

    Add project files.

---
+++ OOP Övning 4/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Övning_4
+{
+    class Class1
+    {
+        private string förnamn;
+        private string efternamn;
+        public int ålder;
+        private double höjd;
+        private double vikt;
+
+        public string metod1
+        {
+            get { return förnamn; }
+            set { förnamn = value; }
+        }
+
+        public string metod2
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+
+        public double metod3
+        {
+            get { return höjd; }
+            set { höjd = value; }
+        }
+
+        public double metod4
+        {
+            get { return vikt; }
+            set { vikt = value; }
+        }
+
+        public bool myndig()
+        {
+            return ålder >= 18;
+        }
+
+        public double BMI()
+        {
+            return vikt / (höjd * höjd);
+        }
+    }
+}
---
+++ OOP Övning 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP_Övning_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p1 = new Class1();
+            p1.metod1 = "Dijar";
+            p1.metod2 = "Zejnullahi";
+
+            Console.WriteLine(p1.metod1 + " " + p1.metod2);
+
+            DateTime födelsedatum = new DateTime(2006, 12, 11);
+            Console.WriteLine(födelsedatum.ToString());
+
+            Console.WriteLine("Hur gammal är du?");
+            p1.ålder = int.Parse(Console.ReadLine());
+            Console.WriteLine("Myndig: " + p1.myndig());
+
+            Class1 p2 = new Class1();
+            Console.WriteLine("Hur lång är du i meter?");
+            p2.metod3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket väger du i kg?");
+            p2.metod4 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Längd: " + p2.metod3);
+            Console.WriteLine("Vikt: " + p2.metod4);
+            Console.WriteLine("BMI: " + p2.BMI());
+        }
+    }
+}
